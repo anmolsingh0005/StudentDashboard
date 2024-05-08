eb6efbdb53840f068719787719a655f10972b80d
anmolsingh0005 <virkanmol074@gmail.com>
2024-05-08 17:48:28 +0530

    test

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api/v1');
-  await app.listen(4000,'0.0.0.0');
+  await app.listen(process.env.PORT,'0.0.0.0');
 }
 bootstrap();
